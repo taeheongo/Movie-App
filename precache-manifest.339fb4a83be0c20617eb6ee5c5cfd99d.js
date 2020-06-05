f75c1e6a36f86362324e8bad7b93eeb4e70f0b88
taeheongo <taeheon9966@gmail.com>
2020-06-05 22:17:03 +0900

    Updates

---
+++ precache-manifest.339fb4a83be0c20617eb6ee5c5cfd99d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37efb6b9440e343be33975cfdf5843e",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "4934832cfa85008452b6",
+    "url": "/movie_app/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "f90da3e3bd152a142992",
+    "url": "/movie_app/static/js/2.b53e3bad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.b53e3bad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4934832cfa85008452b6",
+    "url": "/movie_app/static/js/main.f636fa6d.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
